64cf8658c8a5c07ddc99f1d3f9dfcf1f6c354704
linkviii <LinkEighth@gmail.com>
2018-12-02 15:49:41 -0500

    Add links to chat log

--- viii_amq.js
+++ viii_amq.js
@@ -3,16 +3,30 @@
 // Tested with firefox
 // AMQ uses jquery so might as well go all in with it?
 
-var viii_is_attached;
-function logAnsInChat() {
+// Not sure if using tag id's or .$ references on global objects is 
+// more resilient to change. I'll find out when stuff breaks.
+// I just expect things to break. Its fine.
+
+// Namespacing my global js with 'viii'. 
+// Probably could use an object or something but, eh, keeping it simple.
+
+var viii_isAttached;
+function viii_logAnsInChat() {
 	
+	// For local messages it seems AMQ just injects html.
+	// No reason not to do that I guess. 
+
 	let count = $("#qpCurrentSongCount").text() + " / " + $("#qpTotalSongCount").text();
 	let anime = $("#qpAnimeName").text();
-	let a = count + ": " + anime;
+	
 	let type = $("#qpSongType").text();
 	let song = $("#qpSongName").text();
 	let artist = $("#qpSongArtist").text();
-	let b = type + ": " + song + " |by| " + artist;
+	let linkSrc = $("#qpSongVideoLink")[0].href;
+	let linkTag = '<a href="' + linkSrc + '" target="_blank"> [+] </a>';
+
+	let a = count + ": " + anime + " " + linkTag;
+	let b = type + ": " + song + " <b>[by]</b> " + artist;
 	
 	gameChat.systemMessage(a, b);
 	
@@ -21,13 +35,13 @@ function logAnsInChat() {
 /**
  * Watching qpInfoHider for when results shown.
  */
-function viii_get_watched() {
+function viii_getWatched() {
 	const id = "#qpInfoHider";
 	const changeSelector = id + ".hide";
 	return [$(id).parent(), changeSelector];
 }
 
-function viii_parse_keys(e) {
+function viii_parseKeys(e) {
 	let key = e.keyCode ? e.keyCode : e.which;
 	
 	// Probably a better way to go about this but, it works.
@@ -68,17 +82,17 @@ function viii_parse_keys(e) {
 }
 
 function viii_attatch() {
-	if (viii_is_attached) viii_off();
-	viii_is_attached = true;
-	let [theWatched, onThe] = viii_get_watched();
-	theWatched.on("DOMSubtreeModified", onThe, logAnsInChat );
+	if (viii_isAttached) viii_off();
+	viii_isAttached = true;
+	let [theWatched, onThe] = viii_getWatched();
+	theWatched.on("DOMSubtreeModified", onThe, viii_logAnsInChat );
 	
-	window.onkeyup = viii_parse_keys;
+	window.onkeyup = viii_parseKeys;
 }
 
 function viii_off() {
-	viii_is_attached = false;
-	const theWatched = viii_get_watched()[0];
+	viii_isAttached = false;
+	const theWatched = viii_getWatched()[0];
 	theWatched.off("DOMSubtreeModified");
 	
 	window.onkeyup = null;
